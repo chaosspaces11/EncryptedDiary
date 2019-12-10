ee3426dec535a62ecd72143bee2d8fa749b72adb
Michael Chen <chaosspaces11@gmail.com>
2019-12-10 20:47:35 +1100

    Starting on encrypt function

--- Security.c
+++ Security.c
@@ -3,3 +3,23 @@
 //
 
 #include "Security.h"
+#include "stdlib.h"
+#include "string.h"
+
+#define PASSCODE "deadbeef"
+#define PASSLEN  8
+
+// Takes in a filename and encrypts each byte in accordance with
+// the passcode
+void encrypt(char* fileName)
+{
+
+    char* password = PASSCODE;
+    char* homeDir = getenv("$HOME");
+    char* diaryDir = malloc(sizeof(homeDir) + strlen("/.diary") + 1);
+    memset(diaryDir, 0, strlen(homeDir) + strlen("/.diary/") + strlen(fileName)+ 1);
+    strcat(diaryDir, homeDir);
+    strcat(diaryDir, "/.diary");
+    strcat(diaryDir, "/");
+    strcat(diaryDir, fileName);
+}
