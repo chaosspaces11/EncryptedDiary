9f37bc97c5b226cbd92e3670c79cef1dd8edb77a
Mrinaank Sinha <mrinaanks@gmail.com>
2019-12-10 20:28:25 +1100

    Creation of files needed for Mrinaank's section

---
+++ View.c
@@ -0,0 +1,5 @@
+//
+// Created by Mrinaank Sinha on 10/12/19.
+//
+
+#include "View.h"
---
+++ View.h
@@ -0,0 +1,8 @@
+//
+// Created by Mrinaank Sinha on 10/12/19.
+//
+
+#ifndef ENCRYPTEDDIARY_VIEW_H
+#define ENCRYPTEDDIARY_VIEW_H
+
+#endif //ENCRYPTEDDIARY_VIEW_H
