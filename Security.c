c34dc2fe5fd7ca920b6f3a0eae08ab1cba5de38e
Michael Chen <chaosspaces11@gmail.com>
2019-12-11 21:26:02 +1100

    Basic encryption, currently untested

--- Security.c
+++ Security.c
@@ -3,6 +3,7 @@
 //
 
 #include "Security.h"
+#include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 
@@ -15,11 +16,41 @@ void encrypt(char* fileName)
 {
 
     char* password = PASSCODE;
+
+    // Getting directory set by the $HOME environment variable
     char* homeDir = getenv("$HOME");
-    char* diaryDir = malloc(sizeof(homeDir) + strlen("/.diary") + 1);
-    memset(diaryDir, 0, strlen(homeDir) + strlen("/.diary/") + strlen(fileName)+ 1);
+
+    // Getting space for variable containing the directory of the diary
+    int diaryDirLen = (int)strlen(homeDir) + (int)strlen("/.diary/") + (int)strlen(fileName)+ 1;
+    char* diaryDir = malloc((size_t)diaryDirLen);
+    memset(diaryDir, 0, diaryDirLen);
     strcat(diaryDir, homeDir);
     strcat(diaryDir, "/.diary");
     strcat(diaryDir, "/");
     strcat(diaryDir, fileName);
+
+    FILE* diaryFile = fopen(diaryDir, "r+");
+
+    // Ensuring we got the file
+    if (diaryFile == NULL)
+    {
+        fprintf(stderr, "Encrypt: Diary file %s could not be opened for reading and writing\n", fileName);
+    }
+
+    int currChar = fgetc(diaryFile);
+    int len = 0;
+    while(currChar != EOF)
+    {
+        // Shifts current character back one
+        fseek(diaryFile, -1, SEEK_CUR);
+        // Vigenere cipher encryption
+        currChar = currChar + password[len % PASSLEN];
+        len++;
+
+        // Prints encrypted character
+        fputc(currChar, diaryFile);
+
+        // Getting next character
+        currChar = fgetc(diaryFile);
+    }
 }
