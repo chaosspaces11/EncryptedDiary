3ebc7c28e307015529f827fc781c93729a12f41a
Michael Chen <chaosspaces11@gmail.com>
2019-12-15 15:29:06 +1100

    Added enter function which allows creation of dated text files

--- Enter.c
+++ Enter.c
@@ -7,13 +7,43 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#include "string.h"
+#include "time.h"
+
 void enter()
 {
     char string[BUFSIZ];
-    int response = scanf("%s", string);
-    while(response != 0)
+    time_t currTime = time(NULL);
+    char timeString[80];
+    strftime(timeString, sizeof(timeString), "%d-%m-%y.txt", localtime(&currTime));
+    char* homeDir = getenv("HOME");
+    char fileString[BUFSIZ];
+    memset(fileString, 0, BUFSIZ);
+
+    strcat(fileString, homeDir);
+    strcat(fileString, "/.diary/");
+    strcat(fileString, timeString);
+
+    printf("%s\n", fileString);
+
+    FILE* diaryFile = fopen(fileString, "r");
+    if (diaryFile == NULL)
     {
-        fprintf(stdout, "%s\n", string);
+        fclose(diaryFile);
+        diaryFile = fopen(fileString, "a+");
+        fprintf(diaryFile, "%s\n", timeString);
+    }
+    else
+    {
+        fclose(diaryFile);
+        diaryFile = fopen(fileString, "a+");
+    }
+
+    int response = scanf("%s", string);
+    while(response != 0 && response != EOF)
+    {
+        fprintf(diaryFile, "%s\n", string);
         response = scanf("%s", string);
     }
+    fclose(diaryFile);
 }
